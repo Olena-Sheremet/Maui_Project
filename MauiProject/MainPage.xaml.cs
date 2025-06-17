f2d05b83955630e8ea8e39308667e4397b73b869
lenka.sheremet1978 <lenka.sheremet1978@gmail.com>
2025-06-17 09:49:38 +0300

    student performance graphs with data from a JSON file

--- MauiProject/MainPage.xaml.cs
+++ MauiProject/MainPage.xaml.cs
@@ -1,47 +1,61 @@
 ﻿using LiveChartsCore;
+using LiveChartsCore.Measure;
 using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Painting;
+using MauiProject.Models;
 using MauiProject.Services;
 using SkiaSharp;
 
 namespace MauiProject;
-
 public partial class MainPage : ContentPage
 {
     private readonly StudentDataService _studentService;
+    private Student _student;
+    private List<Course> _courses;
 
-    public MainPage(StudentDataService studentService)
+    public MainPage()
     {
         InitializeComponent();
-        _studentService = studentService;
+        _studentService = new StudentDataService();
     }
 
     protected override async void OnAppearing()
     {
         base.OnAppearing();
 
-        var student = await _studentService.LoadStudentDataAsync();
-
-        if (student == null)
+        _student = await _studentService.LoadStudentDataAsync();
+        if (_student == null)
         {
-            await DisplayAlert("Помилка", "Не вдалося завантажити дані студента.", "OK");
+            await DisplayAlert("Помилка", "Не вдалося завантажити дані студента", "OK");
             return;
         }
 
-        Title = $"Успішність: {student.FirstName} {student.LastName}";
+        _courses = _student.Courses;
 
-        var courseNames = student.Courses.Select(c => c.CourseName).ToList();
-        var averageGrades = student.Courses
-            .Select(c => c.Grades.Average(g => g.GradeValue))
-            .ToArray();
+        var pickerItems = new List<string> { "Загальний графік" };
+        pickerItems.AddRange(_courses.Select(c => c.CourseName));
+        CoursePicker.ItemsSource = pickerItems;
+        CoursePicker.SelectedIndex = 0;
+
+        ShowAverageGradesChart();
+    }
+
+    private void ShowAverageGradesChart()
+    {
+        // Масив середніх значень
+        var averageGrades = _courses.Select(c =>
+        {
+            var gradesValues = c.Grades.Select(g => g.GradeValue);
+            return gradesValues.Any() ? gradesValues.Average() : 0;
+        }).ToArray();
 
         GradesChart.Series = new ISeries[]
         {
             new ColumnSeries<double>
             {
                 Values = averageGrades,
-                Fill = new SolidColorPaint(SKColors.SkyBlue),
-                Stroke = new SolidColorPaint(SKColors.DarkBlue) { StrokeThickness = 2 }
+                Fill = new SolidColorPaint(SKColors.MediumSlateBlue),
+                Name = "Середній бал"
             }
         };
 
@@ -49,10 +63,8 @@ public partial class MainPage : ContentPage
         {
             new Axis
             {
-                Labels = courseNames.ToArray(),
-                LabelsRotation = 15,
-                TextSize = 16,
-                LabelsPaint = new SolidColorPaint(SKColors.Black)
+                Labels = _courses.Select(c => c.CourseName).ToArray(),
+                TextSize = 0 // Приховуємо підписи під графіком
             }
         };
 
@@ -61,10 +73,63 @@ public partial class MainPage : ContentPage
             new Axis
             {
                 MinLimit = 0,
-                MaxLimit = 100,
-                TextSize = 16,
-                LabelsPaint = new SolidColorPaint(SKColors.Black)
+                MaxLimit = 100
             }
         };
+
+        GradesChart.TooltipPosition = TooltipPosition.Auto;
+    }
+
+    private void ShowGradesForSelectedCourse(int selectedIndex)
+    {
+        if (selectedIndex < 1 || selectedIndex > _courses.Count)
+            return;
+
+        var selectedCourse = _courses[selectedIndex - 1];
+
+        var subjectNames = selectedCourse.Grades.Select(g => g.CourseName).ToArray();
+        var gradeValues = selectedCourse.Grades.Select(g => (double)g.GradeValue).ToArray();
+
+        GradesChart.Series = new ISeries[]
+        {
+            new ColumnSeries<double>
+            {
+                Values = gradeValues,
+                Fill = new SolidColorPaint(SKColors.CornflowerBlue),
+                Name = "Оцінка"
+            }
+        };
+
+        GradesChart.XAxes = new Axis[]
+        {
+            new Axis
+            {
+                Labels = subjectNames,
+                TextSize = 0 // Ховаємо підписи під графіком
+            }
+        };
+
+        GradesChart.YAxes = new Axis[]
+        {
+            new Axis
+            {
+                MinLimit = 0,
+                MaxLimit = 100
+            }
+        };
+
+        GradesChart.TooltipPosition = TooltipPosition.Auto;
+    }
+
+    private void CoursePicker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (CoursePicker.SelectedIndex == 0)
+        {
+            ShowAverageGradesChart();
+        }
+        else
+        {
+            ShowGradesForSelectedCourse(CoursePicker.SelectedIndex);
+        }
     }
 }
